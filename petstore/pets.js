a2204e6d1c0760664afb029b2c041c2735d0c270
Min La Wee Chan <u6520189@au.edu>
2025-06-18 14:13:08 +0700

    pet grid instead of one line horizontal scroll, and adding more animals.

--- petstore/pets.js
+++ petstore/pets.js
@@ -1,8 +1,16 @@
 const pets = [
     {"name": "Buddy", "type": "Dog", "age": 3, "img": "img/dogs/dog01.jpg"},
-    {"name": "Buddy", "type": "Dog", "age": 3, "img": "img/dogs/dog02.jpg"},
+    {"name": "Max", "type": "Dog", "age": 2, "img": "img/dogs/dog02.jpg"},
+    {"name": "Rocky", "type": "Dog", "age": 4, "img": "img/dogs/dog03.jpg"},
+    {"name": "Luna", "type": "Dog", "age": 1, "img": "img/dogs/dog04.jpg"},
+    {"name": "Charlie", "type": "Dog", "age": 5, "img": "img/dogs/dog05.jpg"},
+    {"name": "Bella", "type": "Dog", "age": 2, "img": "img/dogs/dog06.jpg"},
     {"name": "Whiskers", "type": "Cat", "age": 2, "img": "img/cats/cat01.jpg"},
-    {"name": "Mittens", "type": "Cat", "age": 2, "img": "img/cats/cat02.jpg"},
+    {"name": "Mittens", "type": "Cat", "age": 3, "img": "img/cats/cat02.jpg"},
+    {"name": "Oliver", "type": "Cat", "age": 1, "img": "img/cats/cat03.jpg"},
+    {"name": "Lily", "type": "Cat", "age": 4, "img": "img/cats/cat04.jpg"},
+    {"name": "Simba", "type": "Cat", "age": 2, "img": "img/cats/cat05.jpg"},
+    {"name": "Nala", "type": "Cat", "age": 3, "img": "img/cats/cat06.jpg"}
 ];
 
 function loadPets() {
